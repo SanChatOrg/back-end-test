8367ba74165bd39b9ba6ae8377800e3cf99085b3
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-12-24 19:15:57 +0900

    Feat: Add APIGateway, Config, Server Project MSA 프로젝트 초기 세팅 - APIGateway: 프런트 클라이언트 요청 관리 프로젝트 - Config: Micro Service 프로젝트 환경설정 관리 프로젝트 - Server: Eureka 클라이언트 사이드 디스커버리 서버 프로젝트

---
+++ APIGateway/src/test/java/com/sanchat/client/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanchat.client.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
