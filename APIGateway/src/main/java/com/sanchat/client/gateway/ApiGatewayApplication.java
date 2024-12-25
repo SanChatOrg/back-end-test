8d6094d0e9ee8600428d793771f368c061cacb21
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-12-25 16:35:00 +0900

    Feat: Modify APIGateway Setting, Add Eureka Server - APIGateway gateway 의존성 추가, 라우팅과 필터 역할만 담당   클라이언트 등록시 라우터 경로 추가 하기
    - Eureka Server 생성 클라이언트 등록시 서버 URI 작성
      eureka.client.service-url.defaultZone=http://localhost:8761/eureka/
    
    Config 클라이언트 및 레포지토리 생성 후 yml 로 통합관리작업 필요

--- APIGateway/src/main/java/com/sanchat/client/gateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/sanchat/client/gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.sanchat.client.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients //추가
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
